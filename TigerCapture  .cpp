b6b16c7b6d32d6f5f3e9dda035c12f80030ac341
KaziSifatAlMaksud <kazi.sifat2013@gmail.com>
2022-07-06 20:01:54 +0600

    TigerCapture

--- TigerCapture  .cpp
+++ TigerCapture  .cpp
@@ -1,3 +1,29 @@
+/*
+Suppose there are n cities connected with each other via particular road. Each road
+has a length w. Exactly in one city m there is a tiger. Mayor of other cities want to
+capture that tiger. Mayor who comes first in m city will capture the tiger. Your task
+is to design an algorithm that finds which city mayor will capture the tiger first.
+You are given number of city n, number of road r, length of each road w and the
+location of the city m that holds the tiger as input. Input will be given in following
+format:
+n
+r
+u v w
+Here, u v w denotes the road between city u and v, length of that road is w. See
+sample input and output for better understanding.
+Sample input Sample output
+n: 4
+r: 4
+0 1 7
+2 3 5
+0 3 10
+2 1 1
+m: 2
+
+Mayor in city 1 will capture
+first.
+
+*/
 #include<stdio.h>
 #include<vector>
 
@@ -16,26 +42,17 @@ struct myqueue
 vector<node> g[100] ;
 int n ;
 int dis[100], par[100] ;
-int print_shortest_path(int src,int dest){
-    if(src == dest){
-        printf("%d", src);
-        return 1;
+ void print (int n, int src, int dis[] ){
+    int min, ind;
+    min = 200;
+    for(int i= 0;i<n;i++){
+        if ((dis[i]<min)&&( i!= src)){
+            min = dis[i];
+            ind =i;
+            }
     }
-    int ok;
-    if(par[dest] != -1){
-        ok = print_shortest_path(src, par[dest]);
-    }
-    else{
-        return -1;
-    }
-    if(ok != -1){
-        printf("-> %d", dest);
-    }
-    else{
-        return -1;
-    }
-    return 1;
-}
+    printf("\n Mayor in City %d Will Captures First\n",ind);
+ }
 int pop()
 {
     int min = 1000000 ;
@@ -73,13 +90,12 @@ void Dijkstra(){
             }
         }
     }
-
 }
 int main()
 {
 
     int u, v, w, i ;
-    int edge ,des;
+    int edge ;
 
     scanf("%d %d", &n, &edge) ;
     for(i = 0 ; i < edge ; i++)
@@ -96,8 +112,7 @@ int main()
         dis[i] = 99999 ;
         par[i] = -1 ;
     }
-    int src = 0 ;
-    scanf("Destination: %d",&des);
+    int src = 2 ;
     dis[src] = 0 ;
     for(i = 0 ; i < n ; i++){
         Q[i].name = i ;
@@ -108,18 +123,8 @@ int main()
     for(i = 0 ; i < n ; i++){
         printf("%d ", dis[i]) ;
 
+
     }
-    for(int i=0;i<n;i++){
-        if(i != src){
-            printf("\n\nshortest path from %d to %d : ",src,i);
-            int found = print_shortest_path(src,i);
-            if (found != -1){
-                printf("\n");
-            }
-            else{
-                printf("No possible path\n");
-            }
-        }
-    }
+    print(n,src,dis);
     return 0 ;
 }
